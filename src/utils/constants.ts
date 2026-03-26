c05e59ba891191e6377031d4fb7a8a80c25fb290
Hicham-Zohbi <hishamzouhbi.dev.science@gmail.com>
2026-03-26 12:47:53 +0200

    update base url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ?? "http://localhost:3000/api";
+  import.meta.env.VITE_API_BASE_URL ?? "https://ecommerce-backend-1-kxpv.onrender.com/api";
 
 export const TAX_RATE = 0.12;
 export const PRODUCTS_PAGE_LIMIT = 12;
